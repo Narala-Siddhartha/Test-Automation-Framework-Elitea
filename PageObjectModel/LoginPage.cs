40163743b4fb127e28089afb09bcc32fb8bdc799
narala_siddhartha <narala_siddhartha@epam.com>
2024-11-18 13:56:50 +0530

    Initial Commit

---
+++ PageObjectModel/LoginPage.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjectModel
+{
+    public class LoginPage
+    {
+        private By username = By.XPath("//input[@name='user-name']");
+        private By password = By.XPath("//input[@name='password']");
+        private By loginButton = By.XPath("//input[@name='login-button']");
+    }
+}
